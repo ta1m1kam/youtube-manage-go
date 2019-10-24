0f72dc6a424f4c46a6f687abb9345c2332322660
TaigaMikami <mikamimikami60@gmail.com>
2019-10-24 15:29:52 +0900

    add search features

--- youtube-manager-nuxt/components/TheHeader.vue
+++ youtube-manager-nuxt/components/TheHeader.vue
@@ -5,5 +5,21 @@
         Youtube Player
       </nuxt-link>
     </div>
+    <div class="navbar-menu">
+      <div class="navbar-start">
+        <div class="navbar-item">
+          <div class="field has-addons">
+            <form action="/search" method="get">
+              <input name="q" class="input" type="text" placeholder="検索">
+              <p class="control">
+                <button type="submit" class="button">
+                  Search
+                </button>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   </nav>
 </template>
